bac734d5fb4d7e22229b0581fe814c63a3b46782
hwld <0701hw@gmail.com>
2025-01-30 16:21:16 +0900

    feat: テーブルをsubgridを使って書き直して、画面幅に応じて天候のテキストが隠れるようにする

--- src/components/specific-forecast.tsx
+++ src/components/specific-forecast.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { css } from "../../styled-system/css";
 import { ForecastDay } from "@/api";
-import { HStack } from "@/components/ui/stack";
 
 /* eslint-disable @next/next/no-img-element */
 type Props = { forecastDay: ForecastDay };
@@ -17,114 +16,128 @@ export async function SpecificForecast({ forecastDay }: Props) {
         overflow: "hidden",
       })}
     >
-      <table>
-        <thead
-          className={css({
-            backgroundColor: "var(--color-gray-200)",
-            fontSize: "12px",
-          })}
-        >
-          <Tr>
-            <Th>時間 (時)</Th>
-            <Th isFullWidth>天候</Th>
-            <Th>気温 (℃)</Th>
-            <Th>湿度 (%)</Th>
-            <Th>降水量 (mm)</Th>
-            <Th lastInRow>風速 (km/h)</Th>
-          </Tr>
-        </thead>
-        <tbody>
-          {forecastDay.hour.map((h, i) => {
-            return (
-              <Tr key={i} isLast={i === forecastDay.hour.length - 1}>
-                <Td>{`${i}`.padStart(2, "0")}</Td>
-                <Td>
-                  <HStack className={css({ gap: "4px" })}>
-                    <img
-                      src={h.condition.icon}
-                      alt="condition"
-                      className={css({ width: "30px", height: "30px" })}
-                    />
-                    <span
-                      className={css({
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                      })}
-                    >
-                      {h.condition.text}
-                    </span>
-                  </HStack>
-                </Td>
-                <Td>{h.temp_c}</Td>
-                <Td>{h.humidity}</Td>
-                <Td>{h.precip_mm}</Td>
-                <Td lastInRow>{h.wind_kph}</Td>
-              </Tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div
+        className={css({
+          display: "grid",
+          gridTemplateColumns: "auto auto minmax(0,1fr) auto auto auto auto",
+        })}
+      >
+        <Tr>
+          <Th>時間</Th>
+          {/* 画面幅が小さいとアイコンが縮んでしまうため、アイコンとテキストで列を分けてnoBorderを使用する */}
+          <Th noBorder>天候</Th>
+          <Th alignStart></Th>
+          <Th>気温</Th>
+          <Th>湿度</Th>
+          <Th>降水量</Th>
+          <Th lastInRow>風速</Th>
+        </Tr>
+        {forecastDay.hour.map((h, i) => {
+          return (
+            <Tr key={i} lastRow={i === forecastDay.hour.length - 1}>
+              <Td>{`${i}`.padStart(2, "0")}:00</Td>
+              {/* 画面幅が小さいとアイコンが縮んでしまうため、アイコンとテキストで列を分けてnoBorderとnoPadding使用する */}
+              <Td noBorder>
+                <img
+                  src={h.condition.icon}
+                  alt="condition"
+                  className={css({ width: "30px", height: "30px" })}
+                />
+              </Td>
+              <Td alignStart noPadding>
+                <span
+                  className={css({
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  })}
+                >
+                  {h.condition.text}
+                </span>
+              </Td>
+              <Td>{h.temp_c}℃</Td>
+              <Td>{h.humidity}％</Td>
+              <Td>{h.precip_mm}mm</Td>
+              <Td lastInRow>{h.wind_kph}km/h</Td>
+            </Tr>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-function Tr({ children, isLast }: { children: ReactNode; isLast?: boolean }) {
+function Tr({ children, lastRow }: { children: ReactNode; lastRow?: boolean }) {
   return (
-    <tr
+    <div
       className={css({
-        borderBottom: isLast ? "" : "1px solid var(--color-gray-300)",
+        display: "grid",
+        gridTemplateColumns: "subgrid",
+        gridColumn: "1 / -1",
+        borderBottom: lastRow ? "" : "1px solid var(--color-gray-300)",
+        alignItems: "center",
       })}
     >
       {children}
-    </tr>
+    </div>
   );
 }
 
 function Th({
   children,
-  isFullWidth,
   lastInRow,
+  alignStart,
+  noBorder,
 }: {
-  children: ReactNode;
-  isFullWidth?: boolean;
+  children?: ReactNode;
   lastInRow?: boolean;
+  alignStart?: boolean;
+  noBorder?: boolean;
 }) {
   return (
-    <th
+    <div
       className={css({
-        fontWeight: "normal",
-        textAlign: "start",
+        textAlign: alignStart ? "start" : "end",
         wordBreak: "keep-all",
         paddingInline: "8px",
         paddingBlock: "4px",
-        width: isFullWidth ? "100%" : "auto",
-        borderRight: lastInRow ? "" : "1px solid",
-        borderColor: "var(--color-gray-300)",
+        borderRight:
+          lastInRow || noBorder ? "" : "1px solid var(--color-gray-300)",
+        backgroundColor: "var(--color-gray-200)",
+        height: "100%",
       })}
     >
       {children}
-    </th>
+    </div>
   );
 }
 
 function Td({
   children,
   lastInRow,
+  alignStart,
+  noPadding,
+  noBorder,
 }: {
   children: ReactNode;
   lastInRow?: boolean;
+  alignStart?: boolean;
+  noPadding?: boolean;
+  noBorder?: boolean;
 }) {
   return (
-    <td
+    <div
       className={css({
-        paddingInline: "8px",
-        borderRight: lastInRow ? "" : "1px solid",
-        textAlign: "end",
-        borderColor: "var(--color-gray-300)",
+        paddingInline: noPadding ? "0px" : "8px",
+        borderRight:
+          lastInRow || noBorder ? "" : "1px solid var(--color-gray-300)",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: alignStart ? "start" : "end",
       })}
     >
       {children}
-    </td>
+    </div>
   );
 }
