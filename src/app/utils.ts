69010d1fd6603f3703be3919e6c736031cab0c02
hwld <0701hw@gmail.com>
2025-02-01 02:46:18 +0900

    refactor: 簡易的なResult型を実装

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -8,3 +8,20 @@ export function searchParamsToObject(
     return { ...result, [key]: value };
   }, {});
 }
+
+export type Result<T, E> =
+  | { status: "ok"; value: T }
+  | { status: "error"; error: E };
+
+export const Result = {
+  ok: <T, E>(value: T): Result<T, E> => ({ status: "ok", value }),
+  err: <T, E>(error: E): Result<T, E> => ({ status: "error", error }),
+};
+
+export function isOk<T, E>(result: Result<T, E>) {
+  return result.status === "ok";
+}
+
+export function isErr<T, E>(result: Result<T, E>) {
+  return result.status === "error";
+}
