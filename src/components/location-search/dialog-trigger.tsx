5a2412a6923f5fb6fb3618c46dda0edcdda0cb37
hwld <0701hw@gmail.com>
2025-02-02 01:05:21 +0900

    feat: Cmd(Ctrl)+kで地域検索ダイアログが開くようにした

--- src/components/location-search/dialog-trigger.tsx
+++ src/components/location-search/dialog-trigger.tsx
@@ -7,9 +7,12 @@ import {
   useDismiss,
   useInteractions,
 } from "@floating-ui/react";
-import { IconSearch } from "@tabler/icons-react";
-import { useState } from "react";
+import { IconCommand, IconSearch } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 import { css } from "../../../styled-system/css";
+import { Kbd } from "@/components/location-search/kbd";
+import { isServer } from "@tanstack/react-query";
+import { HStack } from "@/components/ui/stack";
 
 export function LocationSearchDialogTrigger() {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,6 +34,20 @@ export function LocationSearchDialogTrigger() {
     dismiss,
   ]);
 
+  // ショートカットキーでダイアログを開く
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      if ((e.metaKey || e.ctrlKey) && e.key === "k") {
+        setIsOpen(true);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
   return (
     <>
       <button
@@ -51,19 +68,29 @@ export function LocationSearchDialogTrigger() {
           height: "32px",
           borderRadius: "var(--rounded-sm)",
           gap: "var(--space-sm)",
+          justifyContent: "space-between",
           paddingInline: "var(--space-sm)",
           cursor: "pointer",
         })}
         ref={refs.setReference}
         {...getReferenceProps()}
       >
-        <IconSearch
-          size={20}
-          className={css({ color: "var(--color-gray-500)" })}
-        />
-        <div className={css({ color: "var(--color-gray-500)" })}>
-          地域を検索
-        </div>
+        <HStack className={css({ gap: "var(--space-sm)" })}>
+          <IconSearch
+            size={20}
+            className={css({ color: "var(--color-gray-500)" })}
+          />
+          <div className={css({ color: "var(--color-gray-500)" })}>
+            地域を検索
+          </div>
+        </HStack>
+        <Kbd>
+          <HStack className={css({ gap: "var(--space-xs)" })}>
+            {isMac() ? <IconCommand size={12} /> : "Ctrl"}
+            <span>+</span>
+            <span>K</span>
+          </HStack>
+        </Kbd>
       </button>
       {isOpen ? (
         <LocationSearchDialog
@@ -74,4 +101,11 @@ export function LocationSearchDialogTrigger() {
       ) : null}
     </>
   );
+}
+
+function isMac() {
+  if (isServer) {
+    return true;
+  }
+  return window.navigator.userAgent.toLowerCase().includes("mac");
 }
