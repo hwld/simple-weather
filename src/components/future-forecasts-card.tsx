23df6f38e16d053ac3fc55d25ae415f97fb93cc8
hwld <0701hw@gmail.com>
2025-02-05 16:28:21 +0900

    feat: subgridを使って列を揃える

--- src/components/future-forecasts-card.tsx
+++ src/components/future-forecasts-card.tsx
@@ -19,7 +19,7 @@ export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
     <Card>
       <div className="space-y-4">
         <CardLabel label="これからの天気" />
-        <div className="space-y-1">
+        <div className="gap-2 grid sm:grid-cols-[auto_1fr_auto_auto_auto] grid-cols-[max-content_max-content_max-content_1fr] grid-rows-2">
           {forecastdays.map((forecast) => {
             return (
               <Link
@@ -28,9 +28,9 @@ export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
                   locationId,
                   date: forecast.date,
                 })}
-                className="grid grid-cols-[auto_1fr] gap-2 border rounded-sm border-base-200 py-1 px-2 transition-colors duration-100 hover:bg-base-100 active:bg-base-100 items-start sm:items-end"
+                className="grid col-span-full grid-cols-subgrid grid-rows-[auto_auto] gap-2 sm:grid-rows-1 border rounded-sm border-base-200 p-2 transition-colors duration-100 hover:bg-base-100 active:bg-base-100 place-content-center items-start sm:items-center"
               >
-                <div className="flex flex-col items-end w-[30px]">
+                <div className="flex flex-col items-end row-span-full sm:row-span-1">
                   <div className="text-base-500 text-xs">
                     {format(new Date(forecast.date), "MM/dd")}
                   </div>
@@ -38,31 +38,28 @@ export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
                     {format(new Date(forecast.date), "E", { locale: ja })}
                   </div>
                 </div>
-                <div className="flex flex-col sm:flex-row sm:justify-between gap-1">
-                  <div className="grid grid-cols-[auto_1fr] items-center gap-1">
-                    <img
-                      src={forecast.day.condition.icon}
-                      alt="condition"
-                      className="size-[30px]"
-                    />
-                    <span className="truncate">
-                      {forecast.day.condition.text}
-                    </span>
-                  </div>
-                  <div className="grid grid-cols-[1fr_auto] items-center gap-2 pl-2">
-                    <div className="flex gap-2">
-                      <ForecastValue
-                        icon={IconTemperature}
-                        value={`${forecast.day.maxtemp_c} / ${forecast.day.mintemp_c} ℃`}
-                      />
-                      <ForecastValue
-                        icon={IconUmbrella}
-                        value={`${forecast.day.daily_chance_of_rain} %`}
-                      />
-                    </div>
-                    <IconArrowRight size={18} className="text-sky-500" />
-                  </div>
+                <div className="grid grid-cols-[auto_1fr] items-center gap-1 col-[span_3] sm:col-span-1">
+                  <img
+                    src={forecast.day.condition.icon}
+                    alt="condition"
+                    className="size-[30px]"
+                  />
+                  <span className="truncate">
+                    {forecast.day.condition.text}
+                  </span>
                 </div>
+                <ForecastValue
+                  icon={IconTemperature}
+                  value={`${forecast.day.maxtemp_c} / ${forecast.day.mintemp_c} ℃`}
+                />
+                <ForecastValue
+                  icon={IconUmbrella}
+                  value={`${forecast.day.daily_chance_of_rain} %`}
+                />
+                <IconArrowRight
+                  size={18}
+                  className="text-sky-500 justify-self-end"
+                />
               </Link>
             );
           })}
