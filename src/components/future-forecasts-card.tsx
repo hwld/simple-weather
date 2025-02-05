8d318395fee67e162b560d876ff1cc1eab9b3e95
hwld <0701hw@gmail.com>
2025-02-05 15:42:36 +0900

    refactor: future-forecasts-cardをtailwindで書き換える

--- src/components/future-forecasts-card.tsx
+++ src/components/future-forecasts-card.tsx
@@ -7,10 +7,8 @@ import {
 } from "@tabler/icons-react";
 import { format } from "date-fns";
 import { ja } from "date-fns/locale";
-import { css } from "../../styled-system/css";
 import { type ForecastDay } from "@/backend/weather/schema";
 import { Routes } from "@/routes";
-import { VStack, HStack } from "@/components/ui/stack";
 import { Card, CardLabel } from "@/components/ui/card";
 import Link from "next/link";
 
@@ -19,9 +17,9 @@ type Props = { locationId: string; forecastdays: ForecastDay[] };
 export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
   return (
     <Card>
-      <VStack className={css({ gap: "var(--space-md)" })}>
+      <div className="space-y-4">
         <CardLabel label="これからの天気" />
-        <VStack className={css({ gap: "var(--space-xs)" })}>
+        <div className="space-y-1">
           {forecastdays.map((forecast) => {
             return (
               <Link
@@ -30,77 +28,29 @@ export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
                   locationId,
                   date: forecast.date,
                 })}
-                className={css({
-                  border: "1px solid var(--color-gray-200)",
-                  padding: "var(--space-xs) var(--space-sm)",
-                  borderRadius: "var(--space-xs)",
-                  display: "flex",
-                  alignContent: "center",
-                  gap: "var(--space-sm)",
-                  alignItems: { base: "start", sm: "end" },
-                  transition: "background-color",
-                  transitionDuration: "0.1s",
-                  _supportHover: {
-                    backgroundColor: "var(--color-gray-100)",
-                  },
-                  _active: {
-                    backgroundColor: "var(--color-gray-100)",
-                  },
-                })}
+                className="grid grid-cols-[auto_1fr] gap-2 border rounded-sm border-base-200 py-1 px-2 transition-colors duration-100 hover:bg-base-100 active:bg-base-100 items-start sm:items-end"
               >
-                <VStack
-                  className={css({
-                    alignItems: "end",
-                    width: "30px",
-                    flexShrink: 0,
-                  })}
-                >
-                  <div
-                    className={css({
-                      color: "var(--color-gray-500)",
-                      fontSize: "12px",
-                    })}
-                  >
+                <div className="flex flex-col items-end w-[30px]">
+                  <div className="text-base-500 text-xs">
                     {format(new Date(forecast.date), "MM/dd")}
                   </div>
                   <div>
                     {format(new Date(forecast.date), "E", { locale: ja })}
                   </div>
-                </VStack>
-                <div
-                  className={css({
-                    flexGrow: 1,
-                    display: "flex",
-                    flexDir: "column",
-                    sm: { flexDir: "row", justifyContent: "space-between" },
-                  })}
-                >
-                  <HStack
-                    className={css({ gap: "var(--space-xs)", flexGrow: 1 })}
-                  >
+                </div>
+                <div className="flex flex-col sm:flex-row sm:justify-between gap-1">
+                  <div className="grid grid-cols-[auto_1fr] items-center gap-1">
                     <img
                       src={forecast.day.condition.icon}
                       alt="condition"
-                      className={css({ width: "30px", height: "30px" })}
+                      className="size-[30px]"
                     />
-                    <span
-                      className={css({
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                      })}
-                    >
+                    <span className="truncate">
                       {forecast.day.condition.text}
                     </span>
-                  </HStack>
-                  <HStack
-                    className={css({
-                      gap: "var(--space-sm)",
-                      paddingLeft: "var(--space-sm)",
-                      justifyContent: "space-between",
-                    })}
-                  >
-                    <HStack className={css({ gap: "var(--space-sm)" })}>
+                  </div>
+                  <div className="grid grid-cols-[1fr_auto] items-center gap-2 pl-2">
+                    <div className="flex gap-2">
                       <ForecastValue
                         icon={IconTemperature}
                         value={`${forecast.day.maxtemp_c} / ${forecast.day.mintemp_c} ℃`}
@@ -109,33 +59,24 @@ export async function FutureForecastsCard({ locationId, forecastdays }: Props) {
                         icon={IconUmbrella}
                         value={`${forecast.day.daily_chance_of_rain} %`}
                       />
-                    </HStack>
-                    <IconArrowRight
-                      size={18}
-                      className={css({ color: "var(--color-link)" })}
-                    />
-                  </HStack>
+                    </div>
+                    <IconArrowRight size={18} className="text-sky-500" />
+                  </div>
                 </div>
               </Link>
             );
           })}
-        </VStack>
-      </VStack>
+        </div>
+      </div>
     </Card>
   );
 }
 
 function ForecastValue({ icon: Icon, value }: { icon: Icon; value: string }) {
   return (
-    <HStack
-      className={css({
-        gap: "var(--space-xs)",
-        color: "var(--color-gray-500)",
-        whiteSpace: "nowrap",
-      })}
-    >
+    <div className="grid grid-cols-[auto_1fr] items-center gap-1 text-base-500">
       <Icon size={16} />
       <span>{value}</span>
-    </HStack>
+    </div>
   );
 }
