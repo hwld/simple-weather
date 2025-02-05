4378bb5434248f72b76f017d955daddb173e773c
hwld <0701hw@gmail.com>
2025-02-05 15:07:33 +0900

    refactor: app-headerをtailwindで書き換える

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,60 +1,18 @@
-import { Suspense } from "react";
-import { css } from "../../styled-system/css";
 import { HomePageNavigation } from "@/components/home-page-navigation";
-import { VStack, HStack } from "@/components/ui/stack";
 import { LocationSearchDialogTrigger } from "@/components/location-search/dialog-trigger";
 import { IconCloudFilled } from "@tabler/icons-react";
 
 export function AppHeader() {
   return (
-    <VStack
-      className={css({
-        minW: "0px",
-        gap: "var(--space-lg)",
-        backgroundColor: "var(--color-gray-50)",
-        borderWidth: "0px 1px 1px 1px",
-        borderColor: "var(--color-gray-200)",
-        borderRadius: "0 0 var(--rounded-md) var(--rounded-md)",
-        padding: "var(--space-md)",
-        boxShadow: "var(--shadow-md)",
-      })}
-    >
-      <h1>
-        <HStack
-          className={css({
-            gap: "var(--space-sm)",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-          })}
-        >
-          <HStack
-            className={css({
-              gap: "var(--space-sm)",
-            })}
-          >
-            <IconCloudFilled
-              size={20}
-              className={css({
-                flexShrink: 0,
-                color: "var(--color-primary-500)",
-              })}
-            />
-            <p
-              className={css({
-                fontSize: "16px",
-                fontWeight: "bold",
-                lineHeight: 1,
-              })}
-            >
-              SimpleWeather
-            </p>
-          </HStack>
-          <HomePageNavigation />
-        </HStack>
+    <div className="grid gap-6 p-4 border-x border-b border-base-200 rounded-b-lg bg-base-50 shadow-sm min-w-0 grid-cols-1">
+      <h1 className="flex justify-between flex-wrap gap-2">
+        <div className="grid grid-cols-[auto_1fr] gap-2 items-center">
+          <IconCloudFilled size={20} className="text-primary-500" />
+          <p className="text-base font-bold leading-none">SimpleWeather</p>
+        </div>
+        <HomePageNavigation />
       </h1>
-      <Suspense>
-        <LocationSearchDialogTrigger />
-      </Suspense>
-    </VStack>
+      <LocationSearchDialogTrigger />
+    </div>
   );
 }
--- src/components/home-page-navigation.tsx
+++ src/components/home-page-navigation.tsx
@@ -1,41 +1,26 @@
 "use client";
 
 import Link from "next/link";
-import { css } from "../../styled-system/css";
 import { Routes } from "@/routes";
 import { usePathname } from "next/navigation";
 import { IconHome, IconHomeFilled } from "@tabler/icons-react";
+import { tv } from "tailwind-variants";
+
+const nav = tv({
+  base: "grid grid-cols-[auto_1fr] gap-1 leading-none px-2 rounded-sm font-bold break-keep transition-colors duration-100 h-7 items-center",
+  variants: {
+    isHome: {
+      true: "bg-primary-50 border border-primary-500 text-primary-500",
+      false: "text-base-500 hover:bg-stone-200",
+    },
+  },
+});
 
 export function HomePageNavigation() {
   const currentPath = usePathname();
-
   const isHome = currentPath === Routes.home();
 
-  const className = css({
-    ...(isHome
-      ? {
-          bg: "var(--color-primary-50)",
-          color: "var(--color-primary-500)",
-          border: "1px solid var(--color-primary-500)",
-        }
-      : {
-          color: "var(--color-gray-500)",
-          _hover: {
-            bg: "var(--color-gray-200)",
-          },
-        }),
-    transition: "background",
-    transitionDuration: "0.1s",
-    height: "28px",
-    display: "flex",
-    alignItems: "center",
-    lineHeight: 1,
-    gap: "var(--space-xs)",
-    paddingInline: "var(--space-sm)",
-    borderRadius: "var(--rounded-sm)",
-    fontWeight: "bold",
-    wordBreak: "keep-all",
-  });
+  const className = nav({ isHome });
 
   if (isHome) {
     return (
