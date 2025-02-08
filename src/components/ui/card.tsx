d4913ca1acd4ba7aa40550eb98c732ae6892c4ec
hwld <0701hw@gmail.com>
2025-02-08 13:23:52 +0900

    feat: カードのスタイルを変更

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,7 @@ import { ComponentProps, ReactNode } from "react";
 import { tv, VariantProps } from "tailwind-variants";
 
 const card = tv({
-  base: "bg-base-50 border border-base-300 rounded-lg p-4 shadow-sm min-w-fit overflow-hidden",
+  base: "bg-base-50 rounded-lg p-4 shadow-sm min-w-fit overflow-hidden",
   variants: {
     p: { none: "p-0", md: "p-4" },
   },
@@ -26,7 +26,7 @@ export function CardLabel({ label }: { label: string }) {
 
 export function CardContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-base-200 p-4 sm:p-6 rounded-lg inset-shadow-[0_2px_5px_rgba(0_0_0_/_0.15)] border border-base-300 overflow-auto">
+    <div className="bg-base-200 p-4 sm:p-6 rounded-lg inset-shadow-[0_4px_6px_rgb(0_0_0_/_0.07)] border border-base-300 overflow-auto">
       {children}
     </div>
   );
