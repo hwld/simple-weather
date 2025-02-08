b44f1be5dc8092c6188e06c79c503d21bb74c255
hwld <0701hw@gmail.com>
2025-02-08 13:42:07 +0900

    chore: テストを実行する際にNODE_ENV=testで開発サーバーを立ち上げる

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig, devices } from "next/experimental/testmode/playwright";
 
 export default defineConfig({
-  webServer: { command: "npm run dev", url: "http://localhost:3000" },
+  webServer: {
+    command: "NODE_ENV=test npm run dev",
+    url: "http://localhost:3000",
+  },
   testMatch: "tests/**/*.spec.ts",
   projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
   timeout: 60 * 1000,
